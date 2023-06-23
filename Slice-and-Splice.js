efebb990f132ef8363b00c2edc65f69ad28f915d
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-23 13:48:21 +0300

    Update Slice-and-Splice.js

--- Slice-and-Splice.js
+++ Slice-and-Splice.js
@@ -1,5 +1,6 @@
 function frankenSplice(arr1, arr2, n) {
-  let result = arr2.slice("");
+  let result = arr2.slice
+    // slice is used to copy an array while leaving the original array unaltered.
   result.splice(n,0,...arr1.slice(""));
 
   return result;
